22c3ba327a39dc48e4bc58795e0a26d115c6eff4
arq-gabo <jgabrielguerral@gmail.com>
2020-09-06 15:27:03 -0500

    [FEATURE]Creando animaciones con keyframes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { ListOfCategories } from "./components/ListOfCategoryes";
-import { GlobalStyle } from "./GlobalStyles";
+import { GlobalStyle } from "./styles/GlobalStyles";
 import { ListOfPhotoCards } from "./components/ListOfPhotoCard";
+import { Logo } from "./components/Logo";
 
 export const App = () => (
   <div>
     <GlobalStyle />
+    <Logo />
     <ListOfCategories />
     <ListOfPhotoCards />
   </div>
