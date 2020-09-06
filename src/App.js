8aff6dbfcb95f926b07577847266d875eebd1fab
arq-gabo <jgabrielguerral@gmail.com>
2020-09-06 13:47:51 -0500

    [FEATURE]Creando PhotoCard y usando react-icon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { ListOfCategories } from "./components/ListOfCategoryes";
 import { GlobalStyle } from "./GlobalStyles";
+import { ListOfPhotoCards } from "./components/ListOfPhotoCard";
 
 export const App = () => (
   <div>
     <GlobalStyle />
     <ListOfCategories />
+    <ListOfPhotoCards />
   </div>
 );
