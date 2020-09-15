d5ca252690191fca6fb532e92454082c47cdd93b
arq-gabo <jgabrielguerral@gmail.com>
2020-09-15 17:12:54 -0500

    [FEATURE]Cerrar sesión

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import { useInputValue } from "../../hooks/useInputValue";
-import { Form, Input, Button, Title, Error } from "./styles";
+import { Form, Input, Title, Error } from "./styles";
+import { SubmitButton } from "../SubmitButton";
 
 export const UserForm = ({ error, disabled, onSubmit, title }) => {
   const email = useInputValue("");
@@ -25,7 +26,7 @@ export const UserForm = ({ error, disabled, onSubmit, title }) => {
           type="password"
           {...password}
         />
-        <Button disabled={disabled}>{title}</Button>
+        <SubmitButton disabled={disabled}>{title}</SubmitButton>
       </Form>
       {error && <Error>{error}</Error>}
     </Fragment>
