4817a98ff77dbdc86124ef666e3f5318d132937e
arq-gabo <jgabrielguerral@gmail.com>
2020-09-15 18:41:01 -0500

    [FEATURE]Últimos retoques a las rutas de nuestra aplicación

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { GlobalStyle } from "./styles/GlobalStyles";
 import { Logo } from "./components/Logo";
 import { NavBar } from "./components/Navbar";
@@ -8,36 +8,31 @@ import { Detail } from "./pages/Detail";
 import { Favs } from "./pages/Favs";
 import { User } from "./pages/User";
 import { NoRegisteredUser } from "./pages/NoRegisteredUser";
+import { NotFound } from "./pages/NotFound";
 
-import { Router } from "@reach/router";
-import Context from "./Context";
+import { Router, Redirect } from "@reach/router";
+import { Context } from "./Context";
 
 export const App = () => {
+  const { isAuth } = useContext(Context);
+
   return (
     <div>
       <GlobalStyle />
       <Logo />
       <Router>
+        <NotFound default />
         <Home path="/" />
         <Home path="/pet/:categoryId" />
         <Detail path="/detail/:detailId" />
-      </Router>
+        {!isAuth && <NoRegisteredUser noThrow path="/login" />}
+        {!isAuth && <Redirect noThrow from="/favs" to="/login" />}
+        {!isAuth && <Redirect noThrow from="/user" to="/login" />}
+        {isAuth && <Redirect noThrow from="/login" to="/" />}
 
-      <Context.Consumer>
-        {({ isAuth }) =>
-          isAuth ? (
-            <Router>
-              <Favs path="/favs" />
-              <User path="/user" />
-            </Router>
-          ) : (
-            <Router>
-              <NoRegisteredUser path="/favs" />
-              <NoRegisteredUser path="/user" />
-            </Router>
-          )
-        }
-      </Context.Consumer>
+        <Favs path="/favs" />
+        <User path="/user" />
+      </Router>
 
       <NavBar />
     </div>
---
+++ src/pages/NotFound.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const NotFound = () => <h1>Esta Página No Existe :(</h1>;
