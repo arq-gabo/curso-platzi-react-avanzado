7b8be92b02e516486b33debee73d7b6d831e2301
arq-gabo <jgabrielguerral@gmail.com>
2020-09-14 20:05:47 -0500

    [FEATURE]Mutaciones para registro

--- src/pages/NoRegisteredUser.js
+++ src/pages/NoRegisteredUser.js
@@ -1,12 +1,23 @@
 import React, { Fragment } from "react";
 import Context from "../Context";
 import { UserForm } from "../components/UserForm";
+import { RegisterMutation } from "../container/RegisterMutation";
 
 export const NoRegisteredUser = () => (
   <Context.Consumer>
     {({ activateAuth }) => {
       return (
         <Fragment>
+          <RegisterMutation>
+            {(register) => {
+              const onSubmit = ({ email, password }) => {
+                const input = { email, password };
+                const variables = { input };
+                register({ variables }).then(activateAuth);
+              };
+              return <UserForm title="Registrarse" onSubmit={onSubmit} />;
+            }}
+          </RegisterMutation>
           <UserForm title="Registrarse" onSubmit={activateAuth} />
           <UserForm title="Iniciar Sesión" onSubmit={activateAuth} />
         </Fragment>
