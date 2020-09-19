dafe2f25761b1d5ddd30777647b053aeeb803a24
arq-gabo <jgabrielguerral@gmail.com>
2020-09-19 17:35:25 -0500

    [FEATURE]Testing con Cypress

---
+++ cypress/integration/petgram/test_spect.js
@@ -0,0 +1,24 @@
+/*global describe, it, cy */
+
+describe('Petgram', function () {
+    it('para ver si la pagina funciona', function () {
+        cy.visit('/')
+    })
+
+    it('navegamos a una categoria y vemos fotos', function () {
+        cy.visit('/pet/1')
+        cy.get('article')
+    })
+
+    it('si podemos navegar con la navbar a la home', function () {
+        cy.visit('/pet/1')
+
+        cy.get('nav a').first().click()
+        cy.url().should('include', '/')
+    })
+
+    it('los usuarios no registrado ven el formulario de registro y inicio de sesion al ir a favs', function () {
+        cy.visit('/favs')
+        cy.get('form').should('have.length', 2)
+    })
+})
